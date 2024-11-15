ccbfb83f8d1e70221d98fd646bddbef873759789
Gustavo Christ <gustavo.dev_christ@outlook.com>
2024-11-15 11:06:59 -0300

    fix: configured main file correctly

--- employees-table/employees.sql
+++ employees-table/employees.sql
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `employees`
 --
 
-CREATE TABLE IF NOT EXISTS `employees` (
+CREATE TABLE IF NOT EXISTS `dev`.`employees` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `address` varchar(255) NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `employees` (
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
+INSERT INTO `dev`.`employees` (`id`, `name`, `address`, `salary`) VALUES
 (1, 'Roland Mendel', 'C/ Araquil, 67, Madrid', 5000),
 (2, 'Victoria Ashworth', '35 King George, London', 6500),
 (3, 'Martin Blank', '25, Rue Lauriston, Paris', 8000);
--- www/config.php
+++ www/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'dev');
-define('DB_PASSWORD', 'dev');
-define('DB_NAME', 'dev');
+define("DB_SERVER", "db");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "dev");
+define("DB_NAME", "dev");
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
